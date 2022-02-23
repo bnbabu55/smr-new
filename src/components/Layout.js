66cc95af4cb60360131b1b7ae2df1cc9ea62a719
bnbabu55 <bnbabu@gmail.com>
2022-02-23 11:14:35 +0530

    ver bump

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,7 +7,7 @@ const Layout = ({ isHomePage, children }) => {
     <div data-is-root-path={isHomePage} className="flex flex-col min-h-screen">
       <Header />
 
-      <main className="flex-1 relative">{children}</main>
+      <main className="flex-1">{children}</main>
       <Footer />
     </div>
   )
