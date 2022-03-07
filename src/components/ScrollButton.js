73551d87c78bd6f1dfaa271f8f0a891ed0a62a05
bnbabu55 <bnbabu@gmail.com>
2022-03-07 10:56:12 +0530

    scroll fix

--- src/components/ScrollButton.js
+++ src/components/ScrollButton.js
@@ -43,7 +43,7 @@ const ScrollButton = () => {
           d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z"
           clipRule="evenodd"
           fillRule="evenodd"
-          className="transition duration-300 ease-in-out delay-150 fill-current text-themeYellow-400 group-hover:text-themeAccent"
+          className="transition duration-300 ease-in-out delay-150 fill-current text-themeYellow-400 group-hover:text-themeOrange-400"
         />
       </svg>
     </button>
